87468f748ee94b46d8e714b96f21414e6105aeca
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-31 18:07:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById('nested').remove('target');
 }
+
+function deepestChild(){
+  return document.getElementById('grand-node').nodes[nodes.length- 1];
+}
